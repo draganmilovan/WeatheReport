e1e73d9540433ad7b4f13a9e7c2c2b8f26587c01
Dragan Milovanovic <draganmilovan381@gmail.com>
2017-10-19 18:04:17 +0200

    Created weather data model

---
+++ WeatheReport/WeatherDataModel.swift
@@ -0,0 +1,47 @@
+//
+//  WeatherDataModel.swift
+//  WeatheReport
+//
+//  Created by Dragan Milovanovic on 10/19/17.
+//  Copyright © 2017 Dragan Milovanovic. All rights reserved.
+//
+
+import Foundation
+
+class WeatherData {
+    
+    var temperature: Int = 0
+    var condition: Int = 0
+    var locationName: String = ""
+    var weatherIconName: String = ""
+    
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
