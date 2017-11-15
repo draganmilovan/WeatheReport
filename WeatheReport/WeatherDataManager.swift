3b96e4c9bc708449e9831ed0fe4d99cb1362432d
Dragan Milovanovic <draganmilovan381@gmail.com>
2017-11-15 15:45:50 +0100

    Added Weather Data Manager class for handling all data in project

---
+++ WeatheReport/WeatherDataManager.swift
@@ -0,0 +1,40 @@
+//
+//  WeatherDataManager.swift
+//  WeatheReport
+//
+//  Created by Dragan Milovanovic on 11/15/17.
+//  Copyright © 2017 Dragan Milovanovic. All rights reserved.
+//
+
+import Foundation
+
+class WeatherDataManager {
+    
+    var locationsIDs: [Int] = []
+    var locations: [String] = []
+    var weatherDatas: [WeatherData] = []
+    
+    
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
