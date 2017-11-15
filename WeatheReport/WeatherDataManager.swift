925f82516c5ece967cee9a3df96d07f85c62316c
Dragan Milovanovic <draganmilovan381@gmail.com>
2017-11-15 16:39:43 +0100

    Added properties for calling weather data

--- WeatheReport/WeatherDataManager.swift
+++ WeatheReport/WeatherDataManager.swift
@@ -14,7 +14,13 @@ class WeatherDataManager {
     var locations: [String] = []
     var weatherDatas: [WeatherData] = []
     
+    let weatherURL = "http://api.openweathermap.org/data/2.5/weather"
+    let uvIndexURL = "http://api.openweathermap.org/data/2.5/uvi"
+    let forecastURL = "http://api.openweathermap.org/data/2.5/forecast"
+    let appID = "ac5c2be22a93a78414edcf3ebfd4885e"
+    var paramsUVI : [String : String] = [:]
     
+    weak var timer: Timer?
     
 }
 
